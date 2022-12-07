c721adf51e3f0adeb49ab1c367a4e82b39657394
jeong922 <hwajeong922@naver.com>
2022-12-07 19:12:33 +0900

    비디오 리스트 영상 추가

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { formatAgo, replaceString } from '../util/date';
 import ChannelInfo from './ChannelInfo';
 import { useNavigate } from 'react-router-dom';
 
 export default function Video({ video, type }) {
+  const [showVideo, setShowVideo] = useState(false);
   const {
     thumbnails,
     title,
@@ -13,22 +14,41 @@ export default function Video({ video, type }) {
     description,
   } = video.snippet;
   const navigate = useNavigate();
+  const handleMouseOver = () => setShowVideo(true);
+  const handleMouseLeave = () => setShowVideo(false);
+  console.log(showVideo);
   return (
     <li
       className={`${
         type === 'list'
           ? 'flex flex-col sm:flex-col md:flex-row'
           : 'px-2 pt-2 cursor-pointer hover:scale-110 duration-300 ease-in-out transform hover:z-20 hover:shadow-lg rounded-xl  dark:hover:bg-darkModeGray'
-      } cursor-pointer`}
+      } cursor-pointer relative`}
       onClick={() => {
         navigate(`/watch?v=${video.id}`);
       }}
+      onMouseEnter={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
     >
       <img
         className={`${type === 'list' ? 'mr-3 mb-3' : 'w-full '} rounded-xl`}
         src={thumbnails.medium.url}
         alt={title}
       />
+      {showVideo && (
+        <iframe
+          className={`${
+            type === 'list'
+              ? 'mr-3 mb-3 rounded-xl w-full sm:w-full md:w-[320px] absolute'
+              : 'w-full'
+          } absolute aspect-video top-0 left-0`}
+          type='text/html'
+          src={`https://www.youtube.com/embed/${video.id}?autoplay=1&mute=1`}
+          title='youtube video player'
+          frameBorder='0'
+          allow='autoplay'
+        ></iframe>
+      )}
       <div className='my-2'>
         {type !== 'list' ? (
           <div className='flex justify-between'>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -14,7 +14,7 @@ export default function Videos({ isLoading, error, data, type }) {
             type === 'list'
               ? 'flex flex-col'
               : 'grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-4'
-          } lg:pl-36 sm:pl-7 pt-2`}
+          } lg:pl-36 sm:pl-7 pt-2 pb-6`}
         >
           {data.map((video) => (
             <Video key={video.id} video={video} type={type} />
